5a2e1104ed47c4475953ce248594f33969380421
Tetiana <tetianaprivat@gmail.com>
2020-04-17 18:37:00 +0300

    Updates

---
+++ precache-manifest.e7a21912d172ac65b60acc69b42507cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c4045e83142e3bbd5e4487946a3005f",
+    "url": "/hw6-react-redux.git/index.html"
+  },
+  {
+    "revision": "913bd76f4764c2fb9e8a",
+    "url": "/hw6-react-redux.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2acb856c0416808eb863",
+    "url": "/hw6-react-redux.git/static/js/2.78bb1f37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw6-react-redux.git/static/js/2.78bb1f37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "913bd76f4764c2fb9e8a",
+    "url": "/hw6-react-redux.git/static/js/main.530ee422.chunk.js"
+  },
+  {
+    "revision": "b1d8666b4d12e1be2353",
+    "url": "/hw6-react-redux.git/static/js/runtime-main.ad9e9186.js"
+  }
+]);
